aa7e00ae94a1ad8ee9af262b7cf108be0f2c39dc
Gabriel Duarte <gabrield.developer@gmail.com>
2020-06-06 01:31:31 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const server = express()
+const db = require("./database/db")
 
 // configurar pasta pública
 server.use(express.static("public"))
+
+// habilitar o uso do req.body na aplicação
+
+server.use(express.urlencoded({ extended: true }))
 
 
 // utilizando template engine
@@ -21,14 +26,79 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+
+
     
     return res.render("create-point.html")
 })
 
-server.get("/search", (req, res) => {   
-    
-    return res.render("search-results.html")
+server.post("/savepoint", (req, res) => {
 
+    //console.log(req.body)
+    // inserir dados no banco de dados
+        const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro")
+        } 
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+   
+})
+
+server.get("/search", (req, res) => {   
+
+    const search = req.query.search
+    if (search == "") {
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+
+    // pegar dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+
+        const total = rows.length
+
+        return res.render("search-results.html", { places: rows, total})
+    })
 })
 // ligar o servidor
 server.listen(3000)
